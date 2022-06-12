0f75593f79af12f92a79bf0bc0af652f1c1f07ab
Petr Bouchal <pbouchal@gmail.com>
2022-06-13 00:42:16 +0200

    add fn for loading regional age-gender pay data

--- R/pv_load_reg.R
+++ R/pv_load_reg.R
@@ -61,3 +61,30 @@ pv_reg_monthlypay_isco4 <- function(path) {
   return(ispv_all)
 }
 
+pv_reg_monthlypay_age_gender <- function(path) {
+
+  names(path) <- path
+
+  ispv_all_list <- purrr::map(path, function(x) {
+
+    dt <- readxl::read_excel(x, sheet = 2, skip = 11, n_max = 34-11,
+                             col_names = c("category", "blank1", "blank2",
+                                           colnames_monthly_pay_withmedianyoy))
+    dt$file <- x
+    dt <- dt[!is.na(dt$category),]
+    dt$blank1 <- NULL
+    dt$blank2 <- NULL
+    dt$group <- c(rep("All", 7), rep("Male", 7), rep("Female", 7))
+
+    dt[,3:13] <- suppressWarnings(sapply(dt[,3:13], as.numeric))
+
+    return(dt)
+  })
+
+  ispv_all <- do.call(rbind, ispv_all_list)
+  ispv_all <- add_metadata_general(ispv_all)
+  ispv_all <- add_metadata_reg(ispv_all)
+  ispv_all <- tibble::as_tibble(ispv_all)
+
+  return(ispv_all)
+}
